2f2911767d9112d68a0858dc7c8bc2ea69845aa1
Patrick Kim <pkim050@gmail.com>
2019-05-26 00:25:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, eventName) {
+  newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(`Thank you #{array[i]}, for the wonderful #{eventName} gift!`);
+  }
+  return newArray;
+}
+
+function countdown(startingNumber) {
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
